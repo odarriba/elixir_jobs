42532499c8be05798f609464600df3d27077c089
Óscar de Arriba <odarriba@gmail.com>
2017-09-11 16:44:03 +0200

    Add Ecto Enum library

--- mix.exs
+++ mix.exs
@@ -36,11 +36,13 @@ defmodule ElixirJobs.Mixfile do
       {:phoenix, "~> 1.3.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
+      {:ecto, "~> 2.1.0"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:ecto_enum, "~> 1.0"}
     ]
   end
 
