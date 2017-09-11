4b562b716f83e016c4a675109b18504729042655
Óscar de Arriba <odarriba@gmail.com>
2017-09-11 17:09:11 +0200

    Add extra validations

--- lib/elixir_jobs/offers/offer.ex
+++ lib/elixir_jobs/offers/offer.ex
@@ -1,7 +1,12 @@
 defmodule ElixirJobs.Offers.Offer do
   use Ecto.Schema
   import Ecto.Changeset
-  alias ElixirJobs.Offers.Offer
+
+  alias ElixirJobs.{
+    Offers.Offer,
+    EctoEnums.JobTime,
+    EctoEnums.JobType
+  }
 
 
   @primary_key {:id, :binary_id, autogenerate: true}
@@ -12,14 +17,27 @@ defmodule ElixirJobs.Offers.Offer do
     field :location, :string
     field :url, :string
 
+    field :job_time, JobTime
+    field :job_type, JobType
+
     timestamps()
   end
+
+  @required_attrs [:title, :description, :location, :url, :job_time, :job_type]
+  @optional_attrs []
+  @attributes @required_attrs ++ @optional_attrs
 
   @doc false
   def changeset(%Offer{} = offer, attrs) do
     offer
-    |> cast(attrs, [:title, :description, :location, :url])
-    |> validate_required([:title, :location, :description, :url])
+    |> cast(attrs, @attributes)
+    |> validate_required(@required_attrs)
+    |> validate_length(:title, min: 10, max: 80)
+    |> validate_length(:description, min: 10, max: 500)
+    |> validate_length(:location, min: 3, max: 50)
+    |> validate_length(:url, min: 1, max: 255)
     |> validate_format(:url, ~r/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)
+    |> validate_inclusion(:job_time, JobTime.__valid_values__())
+    |> validate_inclusion(:job_type, JobType.__valid_values__())
   end
 end
--- priv/repo/migrations/20170911140505_create_offers.exs
+++ priv/repo/migrations/20170911140505_create_offers.exs
@@ -4,10 +4,10 @@ defmodule ElixirJobs.Repo.Migrations.CreateOffers do
   def change do
     create table(:offers, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :title, :string, null: false
-      add :description, :string, null: false
-      add :location, :string, null: false
-      add :url, :string, null: false
+      add :title, :string, null: false, size: 80
+      add :description, :string, null: false, size: 500
+      add :location, :string, null: false, size: 50
+      add :url, :string, null: false, size: 255
 
       timestamps()
     end
