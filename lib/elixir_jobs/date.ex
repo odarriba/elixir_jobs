3307a24983cfd0d9fe06a9cb0279a570d268b6b8
Óscar de Arriba <odarriba@gmail.com>
2017-09-12 22:43:25 +0200

    Add date show

---
+++ lib/elixir_jobs/date.ex
@@ -0,0 +1,52 @@
+defmodule ElixirJobs.Date do
+  @seconds_per_day 24 * 60 * 60
+
+  def diff(date1, date2) do
+    date1 = date1 |> castin()
+    date2 = date2 |> castin()
+    case Calendar.DateTime.diff(date1, date2) do
+      {:ok, seconds, _, :before} -> -1 * seconds
+      {:ok, seconds, _, _} -> seconds
+      _ -> nil
+    end
+  end
+
+  def strftime(date, format) do
+    {:ok, string} =
+      date
+      |> castin()
+      |> Calendar.Strftime.strftime(format)
+    string
+  end
+
+  def days_ago(count) do
+    Ecto.DateTime.utc
+    |> days_ago(count)
+  end
+
+  def days_ago(date, count) do
+    date
+    |> castin()
+    |> Calendar.DateTime.subtract!(count * @seconds_per_day)
+    |> castout()
+  end
+
+  # Casts Ecto.DateTimes coming into this module
+  defp castin(date) do
+    date
+    |> Ecto.DateTime.to_erl
+    |> Calendar.DateTime.from_erl!("Etc/UTC")
+  end
+
+  # Casts Calendar.DateTime leaving this module back to erl
+  defp castout({:ok, date}) do
+    date
+    |> castout()
+  end
+
+  defp castout(date) do
+    date
+    |> Calendar.DateTime.to_erl
+    |> Ecto.DateTime.cast!
+  end
+end
--- lib/elixir_jobs_web/views/view_helpers.ex
+++ lib/elixir_jobs_web/views/view_helpers.ex
@@ -21,4 +21,38 @@ defmodule ElixirJobsWeb.ViewHelpers do
     |> Earmark.as_html!
     |> sanitize
   end
+
+  @doc "Returns a date formatted for humans."
+  def human_readable_date(date, use_abbrevs? \\ true) do
+    if use_abbrevs? && this_year?(date) do
+      cond do
+        today?(date) ->
+          "Today"
+        yesterday?(date) ->
+          "Yesterday"
+        true ->
+          ElixirJobs.Date.strftime(date, "%e %b")
+      end
+    else
+      ElixirJobs.Date.strftime(date, "%e %b %Y")
+    end
+  end
+
+  @doc "Returns a date formatted for RSS clients."
+  def xml_readable_date(date) do
+    Date.strftime(date, "%e %b %Y %T %z")
+  end
+
+  defp this_year?(date), do: date.year == Ecto.DateTime.utc.year
+
+  defp today?(date) do
+    now = Ecto.DateTime.utc
+    date.day == now.day && date.month == now.month && date.year == now.year
+  end
+
+  def yesterday?(date) do
+    now = Ecto.DateTime.utc
+    difference = ElixirJobs.Date.diff(now, date)
+    difference < 2 * 24 * 60 * 60 && difference > 1 * 24 * 60 * 60
+  end
 end
