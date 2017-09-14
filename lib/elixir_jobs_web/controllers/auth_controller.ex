ee5ee3e736bec2d7911188c521a31b5fb846a584
Óscar de Arriba <odarriba@gmail.com>
2017-09-14 23:14:52 +0200

    Add auth controller

---
+++ lib/elixir_jobs_web/controllers/auth_controller.ex
@@ -0,0 +1,24 @@
+defmodule ElixirJobsWeb.AuthController do
+  use ElixirJobsWeb, :controller
+
+  alias ElixirJobs.Users
+
+  plug :scrub_params, "auth" when action in [:create]
+
+  def new(conn, _params) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"auth" => auth_params}) do
+    with {:ok, email} <- Map.fetch(auth_params, "email"),
+         {:ok, password} <- Map.fetch(auth_params, "password"),
+         {:ok, admin} <- Users.auth_admin(email, password) do
+        conn
+        |> redirect(to: offer_path(:index))
+    else
+      _ ->
+        conn
+        |> put_flash(:error, "Invalid credentials!")
+    end
+  end
+end
