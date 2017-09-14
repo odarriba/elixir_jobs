273da07ce5af89f7248b8a5ace55d91fe4642749
Óscar de Arriba <odarriba@gmail.com>
2017-09-14 20:55:45 +0200

    Add view to show alerts

--- lib/elixir_jobs_web/views/shared_view.ex
+++ lib/elixir_jobs_web/views/shared_view.ex
@@ -1,3 +1,9 @@
 defmodule ElixirJobsWeb.SharedView do
   use ElixirJobsWeb, :view
+
+  def get_flash_messages(%Plug.Conn{} = conn) do
+    conn
+    |> Phoenix.Controller.get_flash()
+    |> Map.values
+  end
 end
