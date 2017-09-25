0ae823a44e5a5b0208aaa58de283ba8fac3dd55b
Óscar de Arriba <odarriba@gmail.com>
2017-09-25 18:39:36 +0200

    Fix import.

--- lib/elixir_jobs_web/views/email_view.ex
+++ lib/elixir_jobs_web/views/email_view.ex
@@ -1,5 +1,5 @@
 defmodule ElixirJobsWeb.EmailView do
   use ElixirJobsWeb, :view
 
-  import ElixirJobsWeb.OfferView
+  import ElixirJobsWeb.OfferView, only: [human_get_place: 2, human_get_type: 2]
 end
