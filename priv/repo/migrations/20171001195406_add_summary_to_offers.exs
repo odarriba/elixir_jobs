272933c2a3f99c4a5c159aca4b0dcaf04d488aaa
Kirk Agbenyegah <savekirk@gmail.com>
2017-10-04 09:47:59 +0300

    Add character limit to summary

--- priv/repo/migrations/20171001195406_add_summary_to_offers.exs
+++ priv/repo/migrations/20171001195406_add_summary_to_offers.exs
@@ -3,7 +3,7 @@ defmodule ElixirJobs.Repo.Migrations.AddSummaryToOffers do
 
   def change do
     alter table(:offers) do
-      add :summary, :string
+      add :summary, :string, size: 200
     end
 
   end
