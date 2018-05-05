bf6f9e3a6905f4d676df032b2fffb12f23ba2a47
Óscar de Arriba <odarriba@gmail.com>
2018-05-05 23:31:28 +0200

    Update login and error helpers

--- lib/elixir_jobs_web/views/error_helpers.ex
+++ lib/elixir_jobs_web/views/error_helpers.ex
@@ -10,7 +10,7 @@ defmodule ElixirJobsWeb.ErrorHelpers do
   """
   def error_tag(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
-      content_tag(:span, translate_error(error), class: "help-block error-block")
+      content_tag(:p, translate_error(error), class: "help is-danger")
     end)
   end
 
