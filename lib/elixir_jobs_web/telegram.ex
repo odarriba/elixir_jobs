665b9d4ae11adaf9b87de22122ea72e6ab61aa75
Óscar de Arriba <odarriba@gmail.com>
2018-05-06 16:48:35 +0200

    Add telegram module to send offers there

---
+++ lib/elixir_jobs_web/telegram.ex
@@ -0,0 +1,39 @@
+defmodule ElixirJobsWeb.Telegram do
+  @moduledoc """
+  Module to send new elixir job offers to telegram channel.
+  """
+
+  alias ElixirJobs.Offers.Offer
+  alias ElixirJobsWeb.Router.Helpers, as: Routehelpers
+  alias ElixirJobsWeb.HumanizeHelper
+  alias ElixirJobsWeb.Gettext
+
+  require ElixirJobsWeb.Gettext
+
+  def send(%Plug.Conn{} = conn, %Offer{} = offer) do
+    channel = Application.get_env(:elixir_jobs, :telegram_channel, "")
+    send(conn, offer, channel)
+  end
+
+  def send(_, _, ""), do: :ok
+
+  def send(%Plug.Conn{} = conn, %Offer{} = offer, channel) do
+    job_type = HumanizeHelper.human_get_type(offer.job_type, Gettext.gettext("Unknown"))
+    job_place = HumanizeHelper.human_get_place(offer.job_place, Gettext.gettext("Unknown"))
+
+    text = """
+    *#{offer.title}*
+    #{offer.company} (#{offer.location})
+    #{job_type} - #{job_place}
+    #{Routehelpers.offer_url(conn, :show, offer.slug)}
+    """
+
+    case Nadia.send_message("@#{channel}", text, parse_mode: "Markdown") do
+      {:ok, _result} ->
+        :ok
+
+      error ->
+        error
+    end
+  end
+end
