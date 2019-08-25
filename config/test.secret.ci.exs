9b273cb4b3fe4beff3fc4a613a82bc5744ef3c93
Óscar de Arriba <odarriba@gmail.com>
2019-08-25 10:12:29 +0200

    Configure CI (#2)

---
+++ config/test.secret.ci.exs
@@ -0,0 +1,11 @@
+use Mix.Config
+
+# Configure your database
+config :elixir_jobs, ElixirJobs.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.fetch_env!("POSTGRES_USERNAME"),
+  password: System.fetch_env!("POSTGRES_PASSWORD"),
+  database: System.fetch_env!("POSTGRES_DB"),
+  hostname: System.fetch_env!("POSTGRES_HOST"),
+  hostname: String.to_integer(System.fetch_env!("POSTGRES_PORT")),
+  pool: Ecto.Adapters.SQL.Sandbox
