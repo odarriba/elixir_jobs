e8c6cbe0a5ccfd438a290894bae1d05c06d49336
Óscar de Arriba <odarriba@gmail.com>
2019-08-25 10:17:03 +0200

    Use correct config

--- config/test.secret.ci.exs
+++ config/test.secret.ci.exs
@@ -7,5 +7,5 @@ config :elixir_jobs, ElixirJobs.Repo,
   password: System.fetch_env!("POSTGRES_PASSWORD"),
   database: System.fetch_env!("POSTGRES_DB"),
   hostname: System.fetch_env!("POSTGRES_HOST"),
-  hostname: String.to_integer(System.fetch_env!("POSTGRES_PORT")),
+  port: String.to_integer(System.fetch_env!("POSTGRES_PORT")),
   pool: Ecto.Adapters.SQL.Sandbox
