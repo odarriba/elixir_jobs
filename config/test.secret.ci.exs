355b9e40e3e880ee44aa5eb8b2b60fd046790cb5
Óscar de Arriba <odarriba@gmail.com>
2020-02-05 19:15:32 +0100

    Update tests

--- config/test.secret.ci.exs
+++ config/test.secret.ci.exs
@@ -7,5 +7,4 @@ config :elixir_jobs, ElixirJobs.Repo,
   password: System.fetch_env!("POSTGRES_PASSWORD"),
   database: System.fetch_env!("POSTGRES_DB"),
   hostname: System.fetch_env!("POSTGRES_HOST"),
-  port: String.to_integer(System.fetch_env!("POSTGRES_PORT")),
   pool: Ecto.Adapters.SQL.Sandbox
