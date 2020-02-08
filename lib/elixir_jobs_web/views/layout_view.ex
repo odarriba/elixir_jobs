dc4a47d106e8710ef82633ad27b79ee300de091b
Óscar de Arriba <odarriba@gmail.com>
2020-02-08 15:55:15 +0100

    Update hero logic

--- lib/elixir_jobs_web/views/layout_view.ex
+++ lib/elixir_jobs_web/views/layout_view.ex
@@ -1,11 +1,9 @@
 defmodule ElixirJobsWeb.LayoutView do
   use ElixirJobsWeb, :view
 
-  import Phoenix.Controller, only: [current_url: 1, controller_module: 1, action_name: 1]
+  import Phoenix.Controller, only: [current_url: 1]
 
   alias ElixirJobsWeb.MicrodataHelper
-  alias ElixirJobsWeb.OfferController
-  alias ElixirJobsWeb.PageController
   alias ElixirJobsWeb.SeoHelper
   alias ElixirJobsWeb.Telegram
 
@@ -13,19 +11,6 @@ defmodule ElixirJobsWeb.LayoutView do
     conn
     |> Phoenix.Controller.get_flash()
     |> Map.values()
-  end
-
-  def show_new_offer_button(conn) do
-    current_controller = controller_module(conn)
-    current_action = action_name(conn)
-
-    case {current_controller, current_action} do
-      {OfferController, :index} -> true
-      {OfferController, :index_filtered} -> true
-      {OfferController, :search} -> true
-      {PageController, :about} -> true
-      _ -> false
-    end
   end
 
   def get_telegram_channel, do: Telegram.get_channel()
