17b6b0e5b2ddce68ee23594f0abc45e96cd6654e
Óscar de Arriba <odarriba@gmail.com>
2020-02-18 19:48:40 +0100

    Add offer_url function

--- lib/elixir_jobs_web/views/offer_view.ex
+++ lib/elixir_jobs_web/views/offer_view.ex
@@ -2,8 +2,15 @@ defmodule ElixirJobsWeb.OfferView do
   use ElixirJobsWeb, :view
 
   alias ElixirJobs.Core
+  alias ElixirJobs.Core.Schemas.Offer
 
   alias ElixirJobsWeb.HumanizeHelper
+
+  @utm_params [
+    {"utm_source", "elixirjobs.net"},
+    {"utm_medium", "job_board"},
+    {"utm_campaign", "elixirjobs.net"}
+  ]
 
   def get_job_place_options(default) do
     Enum.reduce(Core.get_job_places(), [], fn option, acc ->
@@ -23,5 +30,25 @@ defmodule ElixirJobsWeb.OfferView do
 
       acc ++ select_option
     end)
+  end
+
+  def offer_url(%Offer{url: url}) do
+    parsed_url = URI.parse(url)
+
+    query = parsed_url.query || ""
+
+    query_params =
+      query
+      |> URI.query_decoder()
+      |> Enum.reject(fn
+        {"utm_source", _} -> true
+        {"utm_medium", _} -> true
+        {"utm_campaign", _} -> true
+        _ -> false
+      end)
+      |> Kernel.++(@utm_params)
+      |> URI.encode_query()
+
+    URI.to_string(%{parsed_url | query: query_params})
   end
 end
